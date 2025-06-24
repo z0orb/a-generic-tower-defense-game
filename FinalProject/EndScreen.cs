59b58c91d899da03e723f17097a55720e5a0359c
z0orb <jasonmaxmilanadrian@gmail.com>
2025-06-24 21:52:09 +0700

    sprite changes 2 - projectile and some ui

--- FinalProject/EndScreen.cs
+++ FinalProject/EndScreen.cs
@@ -12,16 +12,16 @@ namespace FinalProject {
             base.OnPaint(e);
 
             Graphics dc = this.CreateGraphics();
-            Font font = new Font("Arial", 55);
-            if (GameWorldClass.WaveKeeper.CurrentWave < 10) {
-                dc.DrawString(GameWorldClass.WaveKeeper.CurrentWave.ToString(), font, Brushes.Red, 125, 60);
-            }
-            else if (GameWorldClass.WaveKeeper.CurrentWave < 100 && GameWorldClass.WaveKeeper.CurrentWave >= 10) {
-                dc.DrawString(GameWorldClass.WaveKeeper.CurrentWave.ToString(), font, Brushes.Red, 105, 60);
-            }
-            else {
-                dc.DrawString(GameWorldClass.WaveKeeper.CurrentWave.ToString(), font, Brushes.Red, 80, 60);
-            }
+            //Font font = new Font("Arial", 55);
+            //if (GameWorldClass.WaveKeeper.CurrentWave < 10) {
+            //    dc.DrawString(GameWorldClass.WaveKeeper.CurrentWave.ToString(), font, Brushes.Red, 125, 60);
+            //}
+            //else if (GameWorldClass.WaveKeeper.CurrentWave < 100 && GameWorldClass.WaveKeeper.CurrentWave >= 10) {
+            //    dc.DrawString(GameWorldClass.WaveKeeper.CurrentWave.ToString(), font, Brushes.Red, 105, 60);
+            //}
+            //else {
+            //    dc.DrawString(GameWorldClass.WaveKeeper.CurrentWave.ToString(), font, Brushes.Red, 80, 60);
+            //}
         }
 
         private void EndScreen_Load(object sender, EventArgs e) {
